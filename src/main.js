362fa54c7d4ced78c78c0fd65b84f1727e773515
wanzilin <wanzilin@kingtree.com.cn>
2024-06-21 17:50:39 +0800

    MOD：Webpack 初体验（npm init; npm i webpack webpack-cli; npx webpack ./src/main.js --mode=development）

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: wanzilin
+ * @Date: 2024-06-21 17:26:16
+ * @LastEditors: wanzilin
+ * @LastEditTime: 2024-06-21 17:42:16
+ * @FilePath: \StudyNotes-Webpack\src\main.js
+ */
+import count from "./js/count"
+import sum from "./js/sum"
+
+console.log(count(2, 1))
+console.log(sum(1, 2, 3, 4))
