1bb4c86895d37bfaa4c46ff58ac3082f40093172
wanzilin <wanzilin@kingtree.com.cn>
2024-06-21 18:31:15 +0800

    MOD：添加 css-loader、less-loader、sass-loader（注意：官方下载的包可能缺失，需要手动引入 npm i style-loader -D）

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-/*
- * @Author: wanzilin
- * @Date: 2024-06-21 17:26:16
- * @LastEditors: wanzilin
- * @LastEditTime: 2024-06-21 17:42:16
- * @FilePath: \StudyNotes-Webpack\src\main.js
- */
 import count from "./js/count"
 import sum from "./js/sum"
+import './css/index.css'
+import './less/index.less'
+import './scss/index.scss'
 
 console.log(count(2, 1))
 console.log(sum(1, 2, 3, 4))
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,32 @@ module.exports = {
   // 加载器
   module: {
     rules: [
+      {
+        test: /\.css$/i, // 匹配 css 文件
+        // 使用 style-loader 和 css-loader 加载 css 文件，执行顺序从后往前
+        use: [
+          'style-loader', // 将 css 代码注入到 style 标签中
+          'css-loader' // 将 css 文件转化成 commonjs 模块打包到 js 中
+        ], 
+      },
+      {
+        test: /\.less$/i,
+        use: [
+            'style-loader',
+            'css-loader',
+            // 将 Less 编译成 CSS
+            'less-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+            'style-loader',
+            'css-loader',
+            // 将 Sass 编译成 CSS
+            'sass-loader',
+        ],
+      },
     ]
   },
   // 插件
